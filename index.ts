8ae92f5edeaec358a37f1d59271c31b2562c3bfa
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-02 12:42:28 +0600

    added corsOptions, config;

--- index.ts
+++ index.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
+import config from './config';
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
+app.use(cors(config.corsOptions));
+app.use(express.static('public'));
 
 const run = async () => {
-  await mongoose.connect('mongodb://localhost/music-app');
+  await mongoose.connect(config.database);
 
   app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
