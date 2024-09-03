671e0a6fdaeb888bc442bb0003b6638a78f11be5
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-03 16:16:19 +0600

    added tracksRouter;

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import config from './config';
 import artistsRouter from './routers/artists';
 import albumsRouter from './routers/albums';
+import tracksRouter from './routers/tracks';
 
 const app = express();
 const port = 8000;
@@ -13,6 +14,7 @@ app.use(cors(config.corsOptions));
 app.use(express.static('public'));
 app.use('/artists', artistsRouter);
 app.use('/albums', albumsRouter);
+app.use('/tracks', tracksRouter);
 
 
 const run = async () => {
