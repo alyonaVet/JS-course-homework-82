110c9a3175e157a8a74d7c222acc5d9bd650395a
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-05 10:33:19 +0600

    created model TrackHistory;

---
+++ api-music-app/models/TrackHistory.ts
@@ -0,0 +1,40 @@
+import mongoose, {Types} from 'mongoose';
+import User from './User';
+import Track from './Track';
+
+const Schema = mongoose.Schema;
+
+const TrackHistorySchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    validate: {
+      validator: async (value: Types.ObjectId) => {
+        const user = await User.findById(value);
+        return Boolean(user);
+      },
+      message: 'User does not exist',
+    }
+  },
+  track: {
+    type: Schema.Types.ObjectId,
+    ref: "Track",
+    required: true,
+    validate: {
+      validator: async (value: Types.ObjectId) => {
+        const track = await Track.findById(value);
+        return Boolean(track);
+      },
+      message: 'Track does not exist',
+    }
+  },
+  datetime: {
+    type: Date,
+    required: true,
+  }
+});
+
+const TrackHistory = mongoose.model('TrackHistory', TrackHistorySchema);
+
+export default TrackHistory;
