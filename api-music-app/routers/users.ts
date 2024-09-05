7d39b3ceb58604a82b9394be7a7af3c42316a05e
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-05 09:25:32 +0600

    added router post to users;

---
+++ api-music-app/routers/users.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import User from '../models/User';
+import mongoose from 'mongoose';
+
+const usersRouter = express.Router();
+
+usersRouter.post('/', async (req, res, next) => {
+  try {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
+
+    await user.save();
+    return res.send(user);
+
+  } catch (error) {
+    if (error instanceof mongoose.Error.ValidationError) {
+      return res.status(400).send(error);
+    }
+    return next(error);
+  }
+});
+
+export default usersRouter;
