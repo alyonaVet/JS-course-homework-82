2297f1519b5b14a3eb097a0610feb2f47457fa6a
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-05 17:30:26 +0600

    refactor: added methods to checkPassword, generateToken;

--- api-music-app/models/User.ts
+++ api-music-app/models/User.ts
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
-import {UserFields} from '../types';
+import {UserFields, UserMethods, UserModel} from '../types';
+import {randomUUID} from 'crypto';
 
 const SALT_WORK_FACTOR = 10;
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema<UserFields>({
+const UserSchema = new Schema<UserFields, UserModel, UserMethods>({
   username: {
     type: String,
     required: true,
@@ -21,6 +22,14 @@ const UserSchema = new Schema<UserFields>({
     required: true,
   }
 });
+
+UserSchema.methods.checkPassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.generateToken = function () {
+  this.token = randomUUID();
+};
 
 UserSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
@@ -39,6 +48,6 @@ UserSchema.set('toJSON', {
   }
 });
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model<UserFields, UserModel>('User', UserSchema);
 
 export default User;
