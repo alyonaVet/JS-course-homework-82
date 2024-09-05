f05229e1aad7005ad99845948c05af26ec98320e
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-05 09:25:06 +0600

    created UserSchema;

---
+++ api-music-app/models/User.ts
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import {UserFields} from '../types';
+
+const SALT_WORK_FACTOR = 10;
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema<UserFields>({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+});
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
