3a033053b031a93d756785674e02dd6d9c447057
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-13 10:50:59 +0600

    created types to Album, Track;

--- frontend-music-app/src/types.ts
+++ frontend-music-app/src/types.ts
@@ -1,5 +1,24 @@
 export interface Artist {
   _id: string;
   name: string;
-  image: string;
+  image: string | null;
+}
+
+export interface Album {
+  _id: string;
+  artist:{
+    _id: string;
+    name: string;
+  };
+  title: string;
+  date: number;
+  image: string | null;
+}
+
+export interface Track {
+  _id: string;
+  album: string;
+  title: string;
+  trackNumber: number;
+  duration: string;
 }
