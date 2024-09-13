3fe9d1dbd5fb0e04ce4eed433e04569f8309ad53
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-13 10:59:41 +0600

    added Routes with paths;

--- frontend-music-app/src/App.tsx
+++ frontend-music-app/src/App.tsx
@@ -1,5 +1,10 @@
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
 import AppToolbar from './UI/AppToolbar/AppToolbar';
+import Artists from './features/artists/components/Artists';
+import NotFoundPage from './UI/NotFoundPage/NotFoundPage';
+import Albums from './features/albums/components/Albums';
+import Tracks from './features/tracks/components/Tracks';
 
 const App = () => {
   return (
@@ -7,6 +12,14 @@ const App = () => {
       <header>
         <AppToolbar/>
       </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Artists />} />
+          <Route path="/artists/:artistId/albums" element={<Albums />} />
+          <Route path="/albums/:albumId/tracks" element={<Tracks />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
     </>
   );
 };
