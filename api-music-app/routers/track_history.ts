ebcff771f6ecabac983743c1d737084e374be1d1
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-17 18:11:51 +0600

    HW#86: got Album and Artist; added get request;

--- api-music-app/routers/track_history.ts
+++ api-music-app/routers/track_history.ts
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import TrackHistory from '../models/TrackHistory';
 import User from '../models/User';
 import Track from '../models/Track';
+import Album from '../models/Album';
+import Artist from '../models/Artist';
 
 const trackHistoryRouter = express.Router();
 
@@ -34,13 +36,31 @@ trackHistoryRouter.post('/', async (req, res, next) => {
 
     const track = await Track.findById(trackId);
 
+
     if (!track) {
       return res.status(404).send({error: 'Track not found!'});
+    }
+
+    const album = await Album.findById(track.album);
+
+    if (!album) {
+      return res.status(404).send({error: 'Album not found!'});
+    }
+
+    const artist = await Artist.findById(album.artist);
+
+    if (!artist) {
+      return res.status(404).send({error: 'Artist not found!'});
     }
 
     const trackHistory = new TrackHistory({
       user: user._id,
       track: track._id,
+      trackTitle: track.title,
+      album: album._id,
+      albumTitle: album.title,
+      artist: artist._id,
+      artistName: artist.name,
       datetime: new Date(),
     });
 
@@ -54,5 +74,35 @@ trackHistoryRouter.post('/', async (req, res, next) => {
     return next(error);
   }
 });
+
+trackHistoryRouter.get('/', async (req, res, next) => {
+  try {
+    const headerValue = req.get('Authorization');
+
+    if (!headerValue) {
+      return res.status(401).send({error: 'Header "Authorization" not found!'});
+    }
+
+    const [_bearer, token] = headerValue.split(' ');
+
+    if (!token) {
+      return res.status(401).send({error: 'Token not found!'});
+    }
+
+    const user = await User.findOne({token});
+
+    if (!user) {
+      return res.status(401).send({error: 'Invalid token!'});
+    }
+
+    const trackHistories = await TrackHistory.find({user: user._id}).sort({datetime: -1});
+
+    return res.send(trackHistories);
+
+  } catch (error) {
+    return next(error);
+  }
+});
+
 
 export default trackHistoryRouter;
