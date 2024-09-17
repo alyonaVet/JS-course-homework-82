f915d70e33bde0f8222517fe9a7d50d6b6aeda9c
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-17 14:21:02 +0600

    HW#86: added User, ValidationError, GlobalError;

--- frontend-music-app/src/types.ts
+++ frontend-music-app/src/types.ts
@@ -22,3 +22,35 @@ export interface Track {
   trackNumber: number;
   duration: string;
 }
+
+export interface RegisterCredentials {
+  username: string;
+  password: string;
+}
+
+export interface LoginCredentials {
+  username: string;
+  password: string;
+}
+
+export interface User {
+  _id: string;
+  username: string;
+  token: string;
+}
+
+export interface ValidationError {
+  errors: {
+    [key: string]: {
+      name: string;
+      message: string;
+    }
+  },
+  message: string;
+  name: string;
+  _message: string;
+}
+
+export interface GlobalError {
+  error: string;
+}
