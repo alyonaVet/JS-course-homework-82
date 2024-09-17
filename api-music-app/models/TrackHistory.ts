89a0155dde34d86f7539fe34012fd3b28b22f3af
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-17 18:12:04 +0600

    HW#86: added fields;

--- api-music-app/models/TrackHistory.ts
+++ api-music-app/models/TrackHistory.ts
@@ -1,6 +1,8 @@
 import mongoose, {Types} from 'mongoose';
 import User from './User';
 import Track from './Track';
+import Album from './Album';
+import Artist from './Artist';
 
 const Schema = mongoose.Schema;
 
@@ -29,6 +31,31 @@ const TrackHistorySchema = new Schema({
       message: 'Track does not exist',
     }
   },
+  trackTitle: String,
+  album: {
+    type: Schema.Types.ObjectId,
+    ref: "Album",
+    required: true,
+    validator: async (value: Types.ObjectId) => {
+      const album = await Album.findById(value);
+      return Boolean(album);
+    },
+    message: 'Album does not exist',
+  },
+  albumTitle: String,
+  artist: {
+    type: Schema.Types.ObjectId,
+    ref: "Artist",
+    required: true,
+    validate: {
+      validator: async (value: Types.ObjectId) => {
+        const artist = await Artist.findById(value);
+        return Boolean(artist);
+      },
+      message: 'Artist does not exist',
+    }
+  },
+  artistName: String,
   datetime: {
     type: Date,
     required: true,
