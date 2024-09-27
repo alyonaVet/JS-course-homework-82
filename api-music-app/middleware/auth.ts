a2ab4fa4af2a4242e8bcbb253cb045e512ae21e1
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-27 18:24:39 +0600

    HW#89: added checkUser;

--- api-music-app/middleware/auth.ts
+++ api-music-app/middleware/auth.ts
@@ -30,4 +30,33 @@ const auth = async (req: RequestWithUser, res: Response, next: NextFunction) => 
   return next();
 };
 
+export const checkUser = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+  const headerValue = req.get('Authorization');
+
+  if (!headerValue) {
+    req.user = undefined;
+
+    return next();
+  }
+
+  const [_bearer, token] = headerValue.split(' ');
+
+  if (!token) {
+    req.user = undefined;
+
+    return next();
+  }
+
+  const user = await User.findOne({token});
+
+  if (!user) {
+    req.user = undefined;
+
+    return next();
+  }
+  req.user = user;
+
+  return next();
+};
+
 export default auth;
