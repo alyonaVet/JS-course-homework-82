ea9e349a80919cca53c43cf7d27793efc7c2b9ad
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-27 18:38:12 +0600

    HW#89: added routes to add artist, album,track;

--- frontend-music-app/src/App.tsx
+++ frontend-music-app/src/App.tsx
@@ -1,15 +1,22 @@
 import './App.css';
 import {Route, Routes} from 'react-router-dom';
 import AppToolbar from './UI/AppToolbar/AppToolbar';
-import Artists from './features/artists/components/Artists';
+import Artists from './features/artists/Artists';
 import NotFoundPage from './UI/NotFoundPage/NotFoundPage';
-import Albums from './features/albums/components/Albums';
-import Tracks from './features/tracks/components/Tracks';
+import Albums from './features/albums/Albums';
+import Tracks from './features/tracks/Tracks';
 import Register from './features/users/components/Register';
 import Login from './features/users/components/Login';
 import TrackHistoryPage from './features/track_history/components/TrackHistoryPage';
+import AddArtist from './features/artists/AddArtist';
+import ProtectedRoute from './UI/ProtectedRoute/ProtectedRoute';
+import {useAppSelector} from './app/hooks';
+import {selectUser} from './features/users/usersSlice';
+import AddAlbum from './features/albums/AddAlbum';
+import AddTrack from './features/tracks/AddTrack';
 
 const App = () => {
+  const user = useAppSelector(selectUser);
   return (
     <>
       <header>
@@ -17,13 +24,28 @@ const App = () => {
       </header>
       <main>
         <Routes>
-          <Route path="/" element={<Artists />} />
-          <Route path="/artists/:artistId/albums" element={<Albums />} />
-          <Route path="/albums/:albumId/tracks" element={<Tracks />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/track-history" element={<TrackHistoryPage />} />
-          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/" element={<Artists/>}/>
+          <Route path="/artists/:artistId/albums" element={<Albums/>}/>
+          <Route path="/albums/:albumId/tracks" element={<Tracks/>}/>
+          <Route path="/artists/add-artist"
+                 element={
+                   <ProtectedRoute isAllowed={!!user}>
+                     <AddArtist/>
+                   </ProtectedRoute>}/>
+          <Route path="albums/add-album"
+                 element={
+                   <ProtectedRoute isAllowed={!!user}>
+                     <AddAlbum/>
+                   </ProtectedRoute>}/>
+          <Route path="tracks/add-track"
+                 element={
+                   <ProtectedRoute isAllowed={!!user}>
+                     <AddTrack/>
+                   </ProtectedRoute>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/track-history" element={<TrackHistoryPage/>}/>
+          <Route path="*" element={<NotFoundPage/>}/>
         </Routes>
       </main>
     </>
