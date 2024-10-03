d53e5afc10bc7538aa9e5b923d8f1aaa7bbf4253
alyonaVet <a.v.alenka2015@gmail.com>
2024-10-03 10:56:12 +0600

    HW#91: added GoogleOAuthProvider;

--- frontend-music-app/src/main.tsx
+++ frontend-music-app/src/main.tsx
@@ -6,18 +6,22 @@ import {Provider} from 'react-redux';
 import {persistor, store} from './app/store';
 import {PersistGate} from 'redux-persist/integration/react';
 import {addInterceptors} from './axiosApi';
+import {GoogleOAuthProvider} from '@react-oauth/google';
+import {GOOGLE_CLIENT_ID} from './constants';
 
 addInterceptors(store);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <PersistGate persistor={persistor}>
-          <App/>
-        </PersistGate>
-      </Provider>
-    </BrowserRouter>
-  </StrictMode>,
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <StrictMode>
+      <BrowserRouter>
+        <Provider store={store}>
+          <PersistGate persistor={persistor}>
+            <App/>
+          </PersistGate>
+        </Provider>
+      </BrowserRouter>
+    </StrictMode>
+  </GoogleOAuthProvider>,
 )
 ;
